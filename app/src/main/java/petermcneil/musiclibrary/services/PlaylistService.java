477d590b2853e15ee07350b42a63a00b5a6d9bd4
petermcneil <petermcneil@users.noreply.github.com>
2017-08-08 10:11:08 +0100

    Added testing for PlaylistService

--- app/src/main/java/petermcneil/musiclibrary/services/PlaylistService.java
+++ app/src/main/java/petermcneil/musiclibrary/services/PlaylistService.java
@@ -6,9 +6,11 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class PlaylistService {
     private final Map<Integer, Playlist> playlistDB;
+    private AtomicLong highestInt = new AtomicLong(0);
 
     public PlaylistService(){
         playlistDB = new ConcurrentHashMap<>();
@@ -16,6 +18,10 @@ public class PlaylistService {
 
     public Set<Playlist> getPlaylistList(){
         Set<Playlist> playlistSet = new HashSet();
+
+        for(Map.Entry<Integer, Playlist> entry: playlistDB.entrySet()){
+            playlistSet.add(entry.getValue());
+        }
         return playlistSet;
     }
 
@@ -24,9 +30,17 @@ public class PlaylistService {
     }
 
     public Integer postPlaylist(Playlist playlist){
-        Integer playlistId = playlistDB.size();
+        Long temp = highestInt.getAndIncrement();
+        Integer playlistId = temp.intValue();
         playlistDB.put(playlistId, playlist);
         return playlistId;
     }
 
+    public void putPlaylist(Playlist playlist, Integer playlistId) {
+        playlistDB.put(playlistId, playlist);
+    }
+
+    public void deletePlaylist(Integer playlistId) {
+        playlistDB.remove(playlistId);
+    }
 }
---
+++ app/src/test/java/petermcneil/musiclibrary/services/PlaylistServiceTest.java
@@ -0,0 +1,93 @@
+package petermcneil.musiclibrary.services;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import petermcneil.domain.Playlist;
+import petermcneil.domain.Song;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class PlaylistServiceTest {
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    Song song1 = Song.songBuilder().title("Song 1").build();
+    Song song2 = Song.songBuilder().title("Song 2").build();
+
+    Playlist playlist1 = Playlist.playlistBuilder().title("playlist1").tracks(ImmutableList.of(song1, song2)).build();
+    Playlist playlist2 = Playlist.playlistBuilder().title("playlist2").tracks(ImmutableList.of(song2, song2, song1, song2)).build();
+    Playlist playlist3 = Playlist.playlistBuilder().title("playlist3").tracks(ImmutableList.of(song1, song2, song2, song2)).build();
+
+    @Test
+    public void getPlaylistList() throws Exception {
+        PlaylistService ps = new PlaylistService();
+
+        ps.postPlaylist(playlist2);
+        ps.postPlaylist(playlist1);
+        ps.postPlaylist(playlist1);
+        ps.postPlaylist(playlist3);
+
+        List playlists = new ArrayList(ps.getPlaylistList());
+
+        assertEquals(playlist1, playlists.get(1));
+        assertEquals(playlist2, playlists.get(0));
+        assertEquals(playlist3, playlists.get(2));
+
+    }
+
+    @Test
+    public void getPlaylist() throws Exception {
+        PlaylistService ps = new PlaylistService();
+
+        ps.postPlaylist(playlist1);
+        ps.postPlaylist(playlist2);
+        ps.postPlaylist(playlist3);
+
+        assertTrue(ps.getPlaylist(0) == playlist1);
+        assertTrue(ps.getPlaylist(2) == playlist3);
+    }
+
+    @Test
+    public void postPlaylist() throws Exception {
+        PlaylistService ps = new PlaylistService();
+
+        assertTrue(ps.postPlaylist(playlist1) == 0);
+        assertTrue(ps.postPlaylist(playlist1) == 1);
+    }
+
+    @Test
+    public void putPlaylist() throws Exception {
+        PlaylistService ps = new PlaylistService();
+
+        ps.postPlaylist(playlist1);
+        assertTrue(ps.getPlaylist(0) == playlist1);
+
+        ps.putPlaylist(playlist2, 0);
+        assertTrue(ps.getPlaylist(0) == playlist2);
+    }
+
+    @Test
+    public void deletePlaylist() throws Exception {
+        PlaylistService ps = new PlaylistService();
+
+        ps.postPlaylist(playlist1);
+        ps.postPlaylist(playlist2);
+        ps.postPlaylist(playlist3);
+
+        ps.deletePlaylist(0);
+        assertNull(ps.getPlaylist(0));
+
+        ps.deletePlaylist(2);
+        assertNull(ps.getPlaylist(2));
+
+        assertTrue(ps.getPlaylist(1) == playlist2);
+    }
+
+}

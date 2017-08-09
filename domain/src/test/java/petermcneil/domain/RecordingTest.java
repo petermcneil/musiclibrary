f89b6a4fc50d520527fdc079ca5366d3cc4c2513
petermcneil <peter.john.mcneil@gmail.com>
2017-08-09 17:30:00 +0100

    Refactored the domain test methods.

--- domain/src/test/java/petermcneil/domain/RecordingTest.java
+++ domain/src/test/java/petermcneil/domain/RecordingTest.java
@@ -2,17 +2,31 @@ package petermcneil.domain;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 public class RecordingTest {
-    @Test
-    public void copy() throws Exception {
-        Recording everythingNow = Recording.recordingBuilder()
-                .title("Everthing Now")
+    String string1 = "Everything Now";
+
+    private Recording everythingNow = Recording.recordingBuilder()
+                .title(string1)
                 .artist(Artist.artistBuilder().name("Arcade Fire").build())
                 .type(Recording.RecordType.SINGLE)
                 .build();
 
+    @Test
+    public void exactCopy() throws Exception {
+        Recording somethingNow = everythingNow
+                .copy()
+                .build();
+
+        assertEquals(everythingNow, somethingNow);
+        assertFalse(everythingNow == somethingNow);
+    }
+
+    @Test
+    public void copyAndChange() throws Exception{
         Recording somethingNow = everythingNow
                 .copy()
                 .build();
@@ -23,9 +37,12 @@ public class RecordingTest {
                 .type(Recording.RecordType.LP)
                 .build();
 
-        assertEquals(everythingNow, somethingNow);
-        assertFalse(everythingNow == somethingNow);
         assertNotEquals(somethingNow, nothingNow);
+        assertTrue(somethingNow != nothingNow);
     }
 
+    @Test
+    public void attributeSaving() throws Exception{
+        assertThat(everythingNow.getTitle(), is(equalTo(string1)));
+    }
 }

2d6ed4a8278737125cd24b6c59082e9162fb3307
petermcneil <peter.john.mcneil@gmail.com>
2017-08-10 12:17:06 +0100

    Added logging for Song controller and service

--- app/src/main/java/petermcneil/musiclibrary/controllers/SongController.java
+++ app/src/main/java/petermcneil/musiclibrary/controllers/SongController.java
@@ -1,5 +1,7 @@
 package petermcneil.musiclibrary.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,11 +10,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import petermcneil.domain.Artist;
 import petermcneil.domain.Song;
+import petermcneil.musiclibrary.services.RecordingService;
 import petermcneil.musiclibrary.services.SongService;
 
 @Controller
 public class SongController {
     private final SongService db;
+    private static final Logger LOG = LoggerFactory.getLogger(RecordingService.class);
 
     public SongController(SongService db){
         this.db = db;
@@ -25,24 +29,28 @@ public class SongController {
 
     @RequestMapping(value="/song/{songId}", method = RequestMethod.GET)
     public String getSong(@PathVariable Integer songId, Model model){
+        LOG.info("REQUEST : GET the song at the id: {}", songId);
         model.addAttribute("song", db.getSong(songId));
         return "song";
     }
 
     @RequestMapping(value="/songs", method= RequestMethod.GET)
     public String getSongs(Model model){
+        LOG.info("REQUEST : GET the list of all the songs");
         model.addAttribute("songs", db.getSongList());
-        return "songs";
+        return "songList";
     }
 
     @RequestMapping(value="/song", method = RequestMethod.POST)
     public String postSong(Song song){
+        LOG.info("REQUEST : POST the song ({}) to the library", song.getTitle());
         Integer songId = db.postSong(song);
         return "redirect:/song/{" + songId + "}";
     }
 
     @RequestMapping(value = "/song/{songId}", method = RequestMethod.PUT)
     public String putSong(@PathVariable Integer songId, Song song, Model model){
+        LOG.info("REQUEST : PUT the song ({}) to the id: {}", song.getTitle(), songId);
         db.putSong(songId, song);
         model.addAttribute(song);
         return "song";
@@ -50,6 +58,7 @@ public class SongController {
 
     @RequestMapping(value = "/song/{songId}", method = RequestMethod.DELETE)
     public String deleteSong(@PathVariable Integer songId){
+        LOG.info("REQUEST : DELETE the song at the id: {}", songId);
         boolean worked = db.deleteSong(songId);
         if(worked){
             return "redirect:/songs";
